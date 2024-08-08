668cbaecbaa81b2fa506a0550e4b0f930cb33f78
PrathyushaLakkireddy <prathyusha@vitwit.com>
2024-08-08 15:42:31 +0530

    add module

---
+++ types/codec.go
@@ -0,0 +1,21 @@
+package types
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
+)
+
+// RegisterLegacyAminoCodec registers the necessary interfaces and concrete types
+// on the provided LegacyAmino codec. These types are used for Amino JSON serialization.
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	// legacy.RegisterAminoMsg(cdc, &MsgSetAvailAddress{}, "availblob/MsgSetAvailAddress")
+}
+
+// RegisterInterfaces registers the interfaces types with the interface registry.
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil)) // &MsgSetAvailAddress{},
+
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
