0c4ebf630d01d3001aa3f7e67945528cfdc94edc
PrathyushaLakkireddy <prathyusha@vitwit.com>
2024-09-24 15:09:42 +0530

    fix config path issue

--- chainclient/client_config.go
+++ chainclient/client_config.go
@@ -10,15 +10,19 @@ import (
 
 // Config represents the structure of the config.yaml file
 type Config struct {
-	ValidatorKey string `yaml:"validatorKey"` // It is the keyname of the cosmos validator account to sign the transactions
-	KeyringType  string `yaml:"keyringType"`  // It is the type of the keyring to sign the transactions related to submission of data to avail
+	ValidatorKey   string `yaml:"validatorKey"`   // It is the keyname of the cosmos validator account to sign the transactions
+	KeyringBackend string `yaml:"keyringBackend"` // It is the type of the keyring to sign the transactions related to submission of data to avail
 }
 
 func GetClientConfig() Config {
 	// Read the YAML configuration file
-	data, err := os.ReadFile("config.yaml")
+	wd, err := os.Getwd()
+	fmt.Println("working dirrr........", wd)
+	configpath := fmt.Sprintf(wd + "/chainclient/config.yaml")
+	fmt.Println("comnfig pathhhhh....", configpath)
+	data, err := os.ReadFile(configpath)
 	if err != nil {
-		log.Fatalf("Error reading YAML file: %v", err)
+		log.Println("Error reading YAML file: %v", err)
 	}
 
 	// Initialize a Config struct
@@ -27,11 +31,11 @@ func GetClientConfig() Config {
 	// Unmarshal the YAML data into the Config struct
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
-		log.Fatalf("Error unmarshalling YAML: %v", err)
+		log.Println("Error unmarshalling YAML: %v", err)
 	}
 
 	// Output the configuration values
 	fmt.Printf("ValidatorKey: %s\n", config.ValidatorKey)
-	fmt.Printf("KeyringType: %s\n", config.KeyringType)
+	fmt.Printf("KeyringType: %s\n", config.KeyringBackend)
 	return config
 }
